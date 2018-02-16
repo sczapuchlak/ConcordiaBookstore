28178888d08ada547c838d05e5af569460d772b5
stephanie zap <iamstephanie@gmail.com>
2018-02-16 16:14:35 -0600

    Created layout for home to display forum. Added the for loop to add teh db info for listings into a table. Also created a profile page, and a create new post page. Added a search bar and plan on adding functionality to it in next iteration.

--- ConcordiaBookstore.py
+++ ConcordiaBookstore.py
@@ -1,14 +1,15 @@
 import MySQLdb
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-# from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
 from wtforms import Form, StringField, PasswordField, validators
+from functools import wraps
+
 
 def connection():
     conn = MySQLdb.connect(host="localhost",
                            user = "root",
-                           passwd = "yannique16",
-                           db = "bookexchange2")
+                           passwd = "gikQr6kn",
+                           db = "bookexchange")
 
 
     # Create a Cursor object to execute queries.
@@ -23,16 +24,18 @@ app = Flask(__name__, '/static', static_folder='static',
 # this is so the templates always reload when there are changes made
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-class SignUpForm(Form):
 
+class SignUpForm(Form):
     firstname = StringField('First Name', [validators.DataRequired()])
     lastname= StringField('Last Name', [validators.DataRequired()])
     email = StringField('Email Address', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirmpassword', message='Passwords must match')
+        validators.EqualTo('confirmpassword', message="Passwords must match")
     ])
     confirmpassword = PasswordField('Re-enter Password')
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -40,19 +43,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/home')
-def home():
-    return render_template('home .html')
+def validate_email():
+
+    test = "@csp.edu"
+    email = request.form['email']
+    # create connection
+    c, conn = connection()
+
+    result = c.execute("SELECT * FROM user WHERE  USER_Email = %s", (email,))
+
+    if int(result) > 0:
+        error = "Email exist. Please use a different email";
+        return render_template("signup.html", error=error)
+
+    elif email[-8:] != test:
+        error = "Not a valid CSP email";
+        return render_template("signup.html", error=error)
 
 
 @app.route('/signup.html', methods=["GET", "POST"])
 def signup():
-    form = SignUpForm(request.form)
-    if request.method == "POST" and form.validate():
-        firstname = form.firstname.data
-        lastname = form.lastname.data
-        email = form.email.data
-        password = sha256_crypt.encrypt((str(form.password.data)))
+    test = "@csp.edu"
+
+    #form = SignUpForm(request.form)
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        password = sha256_crypt.encrypt((str(request.form['password'])))
 
         #create connection
         c, conn = connection()
@@ -61,7 +79,18 @@ def signup():
 
         if int(result) > 0:
             error = "Email exist. Please use a different email";
-            #flash("Email exist. Please use a different email")
+            return render_template("signup.html", error=error)
+
+        elif email[-8:] != test:
+            error = "Not a valid CSP email";
+            return render_template("signup.html", error=error)
+
+        elif len(request.form['password']) < 8:
+            error = "Password must be more than 8 charecters";
+            return render_template("signup.html", error=error)
+
+        elif request.form['password'] != request.form['confirmpassword']:
+            error = "Password doesn't match"
             return render_template("signup.html", error=error)
 
         else:
@@ -78,19 +107,99 @@ def signup():
             conn.close()
 
         return redirect(url_for('login', flash=flash))
-    return render_template('signup.html', form=form)
+    return render_template('signup.html')
+    #return render_template('signup.html', form=form)
 
-@app.route('/login.html', methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
+
 
 @app.route('/about.html', methods=['GET'])
 def about():
     return render_template('about.html')
 
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+
+  try:
+
+    if request.method == "POST":
+        #get form values
+        user_email = request.form['email']
+        user_password = request.form['password']
+
+        # create connection
+        c, conn = connection()
+
+        #get email addresss from db
+        result = c.execute("SELECT * FROM user WHERE  USER_Email = %s", (user_email,))
+
+        # get stored password hash from db
+        result = c.fetchone()[1]
+
+        #xompare and verify passwords
+        if sha256_crypt.verify(user_password, result):
+            session['logged_in'] = True
+            session['user_email'] = user_email
+
+            #flash("You are now logged in")
+            msg = "You are now logged in"
+            return render_template("home.html", msg=msg)
+            #return redirect(url_for("login"))
+
+        else:
+            #flash("Invalid credential, try again")
+            error = "Invalid credential, try again"
+            return render_template("login.html", error=error)
+
+    return render_template("login.html")
+
+  except Exception as e:
+      flash(e)
+     # error = "Invalid Credentials, try again"
+      return render_template("login.html")
+
+#check if user is logged in
+def require_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+                return f(*args, **kwargs)
+        else:
+            flash("unauthorized, Please log in")
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/logout')
+def logout():
+    #kill session
+    session.clear()
+    flash("you are now logged out")
+    #msg = "You are now logged in"
+    return redirect(url_for('login'))
+
+@app.route('/home.html', methods=["GET", "POST"])
+@require_logged_in
+def home():
+    # rows = bookForum.query.all()
+    return render_template("home.html",
+                           title='Overview')
+                           # ,rows=rows)
+
+
+@app.route('/profile.html', methods=["GET", "POST"])
+@require_logged_in
+def profile():
+     return render_template("profile.html")
+
+@app.route('/newpost.html', methods=["GET", "POST"])
+@require_logged_in
+def newpost():
+     return render_template("newpost.html")
+
+
+
 if __name__ == '__main__':
     app.secret_key='haha you cant guess my secret key'
     app.run(debug=True)
-
 
 
