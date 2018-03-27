e491b05444998f05ef0e168f75d2ce568ed47143
jacobgast <gastj@csp.edu>
2018-03-26 23:26:50 -0500

    Still getting Bad Requests, but it should be up-to-date with the other files

--- ConcordiaBookstore.py
+++ ConcordiaBookstore.py
@@ -116,18 +116,18 @@ def signup():
         else:
 
             c.execute('''
-                                                                    INSERT INTO user( USER_PW, USER_Email, USER_FName, USER_LName, USER_Cnfrm)
-                                                                    VALUES(%s, %s, %s, %s, %s)''',
-                      (password, email, firstname, lastname, confirm))
-
+                    INSERT INTO user( USER_PW, USER_Email, USER_FName, USER_LName)
+                    VALUES(%s, %s, %s, %s)''',
+                      (password, email, firstname, lastname))
             user_id = conn.insert_id()
             print(user_id)
+
             conn.commit()
 
             c.execute('''
-                                  INSERT INTO student(STU_ID, STU_Address, STU_City, STU_State, STU_Zip, STU_Phone, USER_ID)
-                                  VALUES(%s, 'St. Address', 'City', 'State', 'Zip Code', '(000)000-0000', %s)''',
-                      (studnumber, [user_id]))
+                      INSERT INTO student(STU_ID, STU_Address, STU_City, STU_State, STU_Zip, STU_Phone, USER_ID)
+                      VALUES(%s, 'St. Address', 'City', 'State', 'Zip Code', '(000)000-0000', %s)''',
+            (studnumber, [user_id]))
             conn.commit()
 
             # generate token
@@ -235,6 +235,7 @@ def about():
 
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
+
     try:
 
         if request.method == "POST":
@@ -398,10 +399,15 @@ def profile():
 
     c, conn = connection()
 
-    c.execute("SELECT USER_FName, USER_LName, USER_Email, USER_Rating, STU_Phone "
-              "FROM user,student "
-              "WHERE user.USER_ID = student.USER_ID")
+    email = session['user_email']
 
+    # get User information with JOIN to get User's Phone number
+    c.execute("SELECT USER_FName, USER_LName, USER_Email, USER_Rating, "
+              "STU_ID, STU_Address, STU_City, STU_State, STU_Zip, STU_Phone, user.USER_ID "
+              "FROM user JOIN student ON user.USER_ID=student.USER_ID "
+              "WHERE USER_Email = %s", (email,))
+
+    # assign from SQL statement to an array named prof
     prof = c.fetchall()
 
     for data in prof:
@@ -409,10 +415,122 @@ def profile():
         proLName = data[1]
         proEmail = data[2]
         proRating = data[3]
-        proPhone = data[4]
+        proID = data[10]
+        studId = data[4]
+        proAddy = data[5]
+        proCity = data[6]
+        proState = data[7]
+        proZip = data[8]
+        proPhone = data[9]
         proName = proFName + " " + proLName
 
+    print(prof)
+
+    conn.commit()
+
     return render_template("profile.html", data=prof)
+
+@app.route('/updateProfile.html', methods=["GET", "POST"])
+@require_logged_in
+def updateProfile():
+
+    if request.method == "POST":
+
+
+        c, conn = connection()
+
+        email = session['user_email']
+
+        # get User information with JOIN to get User's information
+        c.execute("SELECT USER_FName, USER_LName, USER_Email, USER_Rating, "
+                  "STU_ID, STU_Address, STU_City, STU_State, STU_Zip, STU_Phone, user.USER_ID "
+                  "FROM user JOIN student ON user.USER_ID=student.USER_ID "
+                  "WHERE USER_Email = %s", (email,))
+
+        pro = c.fetchall()
+
+        for data in pro:
+            proFName = data[0]
+            proLName = data[1]
+            proEmail = data[2]
+            proRating = data[3]
+            proID = data[10]
+            studId = data[4]
+            proAddy = data[5]
+            proCity = data[6]
+            proState = data[7]
+            proZip = data[8]
+            proPhone = data[9]
+            proName = proFName + " " + proLName
+
+        print(pro)
+
+        profFName = request.form['field1']
+        profLName = request.form['field2']
+        profStuID = request.form['field3']
+        profAddy = request.form['field4']
+        profCity = request.form['field5']
+        profState = request.form['field6']
+        profZip = request.form['field7']
+        profPhone = request.form['field8']
+
+        if profFName == '':
+            Fname = proFName
+        else:
+            Fname = profFName
+
+        if profLName == '':
+            Lname = proLName
+        else:
+            Lname = profLName
+
+        if profStuID == '':
+            Stud = studId
+        else:
+            Stud = profStuID
+
+        if profAddy == '':
+            Addy = proAddy
+        else:
+            Addy = profAddy
+
+        if profCity == '':
+            City = proCity
+        else:
+            City = profCity
+
+        if profState == '':
+            State = proState
+        else:
+            State = profState
+
+        if profZip == '':
+            Zip = proZip
+        else:
+            Zip = profZip
+
+        if profPhone == '':
+            Phone = proPhone
+        else:
+            Phone = profPhone
+
+        c.execute('''
+                  UPDATE student
+                  SET STU_ID = %s, STU_Address = %s,
+                  STU_City = %s, STU_State = %s,
+                  STU_Zip = %s, STU_Phone = %s
+                  WHERE student.USER_ID = %s''',
+                  (Stud, Addy, City, State, Zip, Phone, proID, ))
+        conn.commit()
+
+        c.execute('''
+                  UPDATE user SET USER_FName = %s, USER_LName = %s
+                  WHERE USER_Email = %s''',
+                  (Fname, Lname, email, ))
+        conn.commit()
+
+        return redirect("profile.html")
+    return render_template("updateProfile.html")
 
 
 @app.route('/newpost.html', methods=["GET", "POST"])
@@ -491,8 +609,7 @@ def newpost():
         c.execute('''
                  INSERT INTO book (CRS_ID, BK_Publisher, PHT_ID, BK_Sale_Type, BK_Comment, BK_Title, BK_ISBN, BK_Author, BK_Edition )
                  VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
-                  (course_Number, book_publisher, [photo_id], sale_type, book_Comments, listing_title, book_ISBN,
-                   book_Author,
+                  (course_Number, book_publisher, [photo_id], sale_type, book_Comments, listing_title, book_ISBN, book_Author,
                    book_Edition))
         course_id = conn.insert_id()
         conn.commit()
@@ -517,8 +634,9 @@ def listing(list_id):
 
     c, conn = connection()
 
-    c.execute("SELECT USER_FName,USER_LName,USER_ID, LST_ID, LST_Title, LST_SellType, LST_Date,LST_ID "
-              "FROM user,listing "
+    c.execute("SELECT USER_FName,USER_LName, USER_ID, LST_ID, LST_Title, LST_SellType, LST_Date,LST_ID, BK_Author,BK_Edition,BK_Title,"
+              "LST_SellType, BK_Publisher,BK_Comment,BK_ISBN,USER_Rating,course.CRS_ID,course.CRS_Name "
+              "FROM user,listing,book,course "
               "WHERE LST_ID = %s", [list_id])
 
     conn.commit()
@@ -531,74 +649,100 @@ def listing(list_id):
         id = data[2]
         listID = data[3]
         listtitle = data[4]
-        # print(data)
+        listDate= data[6]
+        bookAuthor = data[8]
+        bookEdition = data[9]
+        bookTitle = data[10]
+        listSellType = data[11]
+        bookPublisher = data[12]
+        bookDesc = data[13]
+        bookISBN = data[14]
+        userRating = data[15]
+        courseID = data[16]
+        courseName = data[17]
+        print(data)
 
-    # Pull comments from comments table for display related to selected listing
-    c.execute("SELECT COM_Auth, COM_Date, COM_Body, COM_USER_ID FROM comments WHERE LST_ID = %s", [listID])
-    rows = c.fetchall()
+        # Pull comments from comments table for display related to selected listing
+        c.execute("SELECT COM_Auth, COM_Date, COM_Body, COM_USER_ID FROM comments WHERE LST_ID = %s", [listID])
+        rows = c.fetchall()
 
-    # return render_template("listing.html", data=data, email=email, firstname=firstname, lastname=lastname, listID=listID,
-    #                        listtitle=listtitle, rows=rows)
-    return render_template("listing.html", data=data, firstname=firstname, lastname=lastname, id=id, listID=listID,
-                           listtitle=listtitle, rows=rows)
-
+        return render_template("listing.html", data=data, firstname=firstname, lastname=lastname, listID=listID,
+                               listtitle=listtitle, listDate=listDate,
+                               bookTitle=bookTitle, bookAuthor=bookAuthor, bookEdition=bookEdition,
+                               listSellType=listSellType, bookPublisher=bookPublisher,
+                               bookDesc=bookDesc, bookISBN=bookISBN, userRating=userRating, courseID=courseID,
+                               courseName=courseName, id=id, rows=rows)
 
 @app.route("/submit_comment/<list_id>", methods=["GET", "POST"])
 @require_logged_in
 def submit_comment(list_id):
-    date = datetime.now()
-    msg = request.form['message']
-    email = session['user_email']
+        date = datetime.now()
+        msg = request.form['message']
+        email = session['user_email']
 
-    c, conn = connection()
+        c, conn = connection()
 
-    c.execute("SELECT USER_FName, USER_LName, USER_ID FROM user WHERE USER_Email = %s", [email])
-    result = c.fetchall()
-    for data in result:
-        firstname = data[0]
-        lastname = data[1]
-        id = data[2]
+        c.execute("SELECT USER_FName, USER_LName, USER_ID FROM user WHERE USER_Email = %s", [email])
+        result = c.fetchall()
+        for data in result:
+            firstname = data[0]
+            lastname = data[1]
+            id = data[2]
 
-    auth = firstname + " " + lastname
-    # c.execute('''INSERT INTO comments (LST_ID,COM_Auth,COM_Date,COM_Body,...)
-    #           VALUES (%s, %s, %s, %s, %s)''', (list_id, auth, date, msg, email))
-    c.execute('''INSERT INTO comments (LST_ID,COM_Auth,COM_Date,COM_Body, COM_USER_ID) 
-                  VALUES (%s, %s, %s, %s, %s)''', (list_id, auth, date, msg, id))
-    conn.commit()
+        auth = firstname + " " + lastname
+        # c.execute('''INSERT INTO comments (LST_ID,COM_Auth,COM_Date,COM_Body,...)
+        #           VALUES (%s, %s, %s, %s, %s)''', (list_id, auth, date, msg, email))
+        c.execute('''INSERT INTO comments (LST_ID,COM_Auth,COM_Date,COM_Body, COM_USER_ID) 
+                          VALUES (%s, %s, %s, %s, %s)''', (list_id, auth, date, msg, id))
+        conn.commit()
 
-    conn.close()
+        conn.close()
 
-    return redirect(url_for("/listing", list_id))
-
-
+        return redirect(url_for("/listing", list_id))
 
 
 @app.route('/changepassword.html', methods=["GET", "POST"])
 @require_logged_in
 def changepassword():
-    if request.method == "POST":
-        password = sha256_crypt.encrypt((str(request.form['newPassword'])))
 
-        # # create connection
-        # c, conn = connection()
-        #
-        # result = c.execute("UPDATE password FROM user WHERE  USER_Email = %s", (email,))
+   if request.method == "POST":
 
-        if len(request.form['newPassword']) < 8:
-            error = "Password must be more than 8 characters"
-            return render_template("changepassword.html", error=error)
-
-        elif request.form['newPassword'] != request.form['confirmPassword']:
-            error = "Password doesn't match"
-            return render_template("changepassword.html", error=error)
-
-        elif request.form['newPassword'] == request.form['oldPassword']:
-            error = "Old password cannot match new password"
-            return render_template("signup.html", error=error)
+       oldPassword = request.form['oldPassword']
+       newPassword = request.form['newPassword']
+       confirmPassword = request.form['confirmPassword']
 
 
+        # create connection
+       c, conn = connection()
 
-    return render_template("changepassword.html")
+       if len(newPassword) < 8:
+           error = "Password must be more than 8 characters"
+           return render_template("changepassword.html", error=error)
+
+       elif newPassword != confirmPassword:
+           error = "Password doesn't match"
+           return render_template("changepassword.html", error=error)
+
+       elif newPassword == oldPassword:
+           error = "Old password cannot match new password"
+           return render_template("changepassword.html", error=error)
+       else:
+
+           password = sha256_crypt.encrypt((str(newPassword)))
+
+           email = session['user_email']
+
+
+           c.execute("""
+                     UPDATE user
+                     SET USER_PW=%s
+                     WHERE USER_Email=%s
+                  """, (password, email))
+
+           conn.commit()
+
+   return render_template("changepassword.html")
+
 
 
 @app.route('/pwreset.html', methods=["GET", "POST"])
